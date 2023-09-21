57aaad46f0d074b01f2a30047e4232e62f3bfd4a
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2023-09-21 16:07:20 +0200

    filtre

--- api/routers/drinks.py
+++ api/routers/drinks.py
@@ -1,6 +1,5 @@
 import json
-from typing import Dict
-
+from typing import Dict, Optional
 from fastapi import HTTPException, APIRouter, Query
 from pydantic import BaseModel
 
@@ -62,19 +61,39 @@ async def showdrinks(user_id):
 
 
 @router.get("/search/")
-async def search_drinks(query: str = Query(..., description="Recherchez une boisson par nom ou description")):
+async def search_drinks(query: str = Query(..., description="Recherchez une boisson par nom ou description"),
+                        max_price: Optional[float] = Query(None, description="Filtrez par prix maximum")):
     db_connection, db_cursor = connect_to_database()
 
     try:
         # Utilisez une requête SQL pour rechercher des boissons par nom ou description
+        # Ajoutez des % pour rechercher des correspondances partielles
         search_query = f"%{query}%"
-        query = "SELECT * FROM drink WHERE name LIKE %s OR description LIKE %s"
-        db_cursor.execute(query, (search_query, search_query))
+        query = "SELECT * FROM drink WHERE (name LIKE %s OR description LIKE %s)"
+
+        # Si max_price est spécifié, ajoutez un filtre de prix
+        if max_price is not None:
+            query += " AND price <= %s"
+            db_cursor.execute(query, (search_query, search_query, max_price))
+        else:
+            db_cursor.execute(query, (search_query, search_query))
+
         drinks = db_cursor.fetchall()
 
+        if not drinks:
+            # Si aucune boisson n'est trouvée, renvoyer un message personnalisé
+            if max_price is not None and max_price <= 0:
+                raise HTTPException(
+                    status_code=400, detail="Le filtre de prix est trop bas pour trouver des boissons")
+            else:
+                raise HTTPException(
+                    status_code=404, detail="Aucune boisson trouvée")
+
         return drinks
+    except HTTPException:
+        # Capturer l'exception HTTPException et la répercuter
+        raise
     except Exception as e:
         raise e
     finally:
         close_database_connection()
-
