a7c0c8ce2ba68b1f5d5c202b669086b1c3f9a69b
AyaAid <aya.lakhel11@gmail.com>
2023-09-21 10:51:26 +0200

    update drinks w/ try add price of drinks

--- api/routers/drinks.py
+++ api/routers/drinks.py
@@ -51,9 +51,32 @@ async def showdrinks(user_id):
             (user_id,)
         )
         drinks = cursor.fetchall()
+        drinks_with_prices = []
+        for drink_data in drinks:
+            drink_id, supplement_ids = drink_data
+            supplement_id_list = json.loads(supplement_ids)
+
+            # Récupérer le prix de la boisson
+            cursor.execute("SELECT price FROM drink WHERE id = %s", (drink_id,))
+            drink_price = cursor.fetchone()[0]  # Supposant que la colonne price existe dans la table drinks
+
+            # Calculer le prix total de la boisson en tenant compte des suppléments
+            total_price = drink_price
+            for supplement_id, quantity in supplement_id_list.items():
+                # Récupérer le prix du supplément
+                cursor.execute("SELECT price FROM supplement WHERE id = %s", (supplement_id,))
+                supplement_price = cursor.fetchone()[
+                    0]  # Supposant que la colonne price existe dans la table supplements
+                total_price += supplement_price * quantity
+
+            # Ajouter la boisson avec son prix total à la liste
+            drinks_with_prices.append({
+                "drink_id": drink_id,
+                "total_price": total_price
+            })
         close_database_connection()
         if not drinks:
             raise HTTPException(status_code=404, detail="Aucune boisson trouvée pour cet utilisateur")
-        return {"drinks": drinks}, 200
+        return {"drinks": drinks_with_prices}, 200
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
